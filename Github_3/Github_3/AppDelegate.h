43e29e49b8ae7ddb5f48654c92de27a8b350af68
maomao <245124008@qq.com>
2016-03-16 10:16:37 +0800

    搭建框架

---
+++ Github_3/Github_3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github_3
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github_3/Github_3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github_3
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
